e5800e390254c40673449169a53f0dc056a2983d
Himanshuba9hel <himanshuba9hel@gmail.com>
2026-08-16 12:02:07 +0530

    Identification of User is Done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,4 +7,9 @@ const authRouter = Router();
 POST /api/auth/register
 */
 authRouter.post("/register",authController.register);
+
+/*
+GET /api/auth/get-me
+*/
+authRouter.get("/get-me",authController.getMe);
 export default authRouter;
